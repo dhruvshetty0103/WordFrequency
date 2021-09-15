3d3a65273af86c66a1ec33ab80cfac58993eb375
Dhruv Shetty <dhruvshetty0103@gmail.com>
2021-09-15 18:51:13 +0530

    Refactored:Deleting word avoidable from the paragraph

--- src/main/java/com/hashtable/wordfrequency/WordFrequency.java
+++ src/main/java/com/hashtable/wordfrequency/WordFrequency.java
@@ -1,23 +1,23 @@
 package com.hashtable.wordfrequency;
 
-import java.util.HashSet;
 import java.util.Hashtable;
 import java.util.Map.Entry;
-import java.util.Set;
 
 public class WordFrequency 
 {
+	/* Hash table to store the key and value pair where key's
+	 * are the different words in sentence and paragraph and 
+	 * values to store the count it's frequency
+	 */
 	private Hashtable<String, Integer> table = new Hashtable<String, Integer>();
 
 	public void add(String words)
 	{
 		String[] splited = words.split(" ");
-		Set<String> keys = table.keySet();
-		keys.remove("avoidable");
 		for (String count : splited) 
 		{
 			if(table.containsKey("avoidable"))
-				table.remove("avoidable");
+				table.remove("avoidable");//deleting word avoidable from the paragraph
 			else if(table.containsKey(count)) 
 			{
 				table.put(count, table.get(count) + 1);
@@ -28,15 +28,18 @@ public class WordFrequency
 			}
 		}
 	}
-
-	public void show() {
-		
-		for (Entry<String, Integer> entry : table.entrySet()) {
+	/* Iterating through the HashMap set 
+	 */
+	public void show() 
+	{	
+		for (Entry<String, Integer> entry : table.entrySet()) 
+		{
 			System.out.println(entry.getKey() + "\t" + entry.getValue());
 		}
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		WordFrequency abc = new WordFrequency();
 		abc.add("Paranoids are not paranoid because they are paranoid but because they keep putting themselves"
 				+ " deliberately into paranoid avoidable situations");
